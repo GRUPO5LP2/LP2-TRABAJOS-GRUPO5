40a2c712ea5725e7b397a6ef0bc556f6a2a46db1
GRUPO5LP2 <165744261+GRUPO5LP2@users.noreply.github.com>
2024-06-11 10:28:47 -0500

    Update avance.cpp

--- Sesion 9/Avance del proyecto/1er Avance Proyecto LP 2 PRACT/avance.cpp
+++ Sesion 9/Avance del proyecto/1er Avance Proyecto LP 2 PRACT/avance.cpp
@@ -16,7 +16,7 @@ class Tareas{
 class TareaSimple : public Tareas{
     public:
         TareaSimple(string n, string fi, string ff) : Tareas(n, fi, ff){}
-        void mostrarDetalles(){
+        void mostrarDetalles() override{
             cout << "Tarea simple: " << nombre << " Fecha inicio: " << fecha_inicio << " Fecha fin: " << fecha_fin << endl;
         }
 };
@@ -24,7 +24,7 @@ class TareaSimple : public Tareas{
 class TareaCompuesta : public Tareas{
     public:
         TareaCompuesta(string n, string fi, string ff) : Tareas(n, fi, ff){}
-        void mostrarDetalles(){
+        void mostrarDetalles() override{
             cout << "Tarea compuesta: " << nombre << " Fecha inicio: " << fecha_inicio << " Fecha fin: " << fecha_fin << endl;
         }
 };
@@ -32,14 +32,22 @@ class TareaCompuesta : public Tareas{
 class Hito : public Tareas{
     public:
         Hito(string n, string fi, string ff) : Tareas(n, fi, ff){}
-        void mostrarDetalles(){
+        void mostrarDetalles() override{
             cout << "Hito: " << nombre << " Fecha inicio: " << fecha_inicio << " Fecha fin: " << fecha_fin << endl;
         }
 };
 
 int main(){
-    Tareas* tareas[3];
-    tareas[0] = new TareaSimple("Tarea 1", "01/01/2021", "02/01/2021");
-    tareas[1] = new TareaCompuesta("Tarea 2", "01/01/2021", "02/01/2021");
-    tareas[2] = new Hito("Hito 1", "01/01/2021", "02/01/2021");
+    const int longitud_array = 3;
+    Tareas* tareas[longitud_array];
+    tareas[0] = new TareaSimple("Tarea 1", "01/01/2021", "05/01/2024");
+    tareas[1] = new TareaCompuesta("TareaCompuesta 1", "01/01/2021", "05/01/2024");
+    tareas[2] = new Hito("Hito 1", "01/01/2021", "05/01/2024");
+    tareas[0]->mostrarDetalles();
+    tareas[1]->mostrarDetalles();
+    tareas[2]->mostrarDetalles();
+    cout << " ==== Iterando sobre el array para hacer uso del polimorfismo ===" << endl;
+    for(int i = 0; i < longitud_array; i++){
+        tareas[i]->mostrarDetalles();
+    }
 }
